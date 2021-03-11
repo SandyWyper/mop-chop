e5265e93d9947b9f3a93936b5eac061384f083c5
sandywyper <sandywyper@gmail.com>
2021-03-11 15:48:46 +0000

    Update API key

--- src/apiLoad.js
+++ src/apiLoad.js
@@ -3,7 +3,7 @@ const loadGoogleMapsApi = require('load-google-maps-api');
 class MapApi {
   static loadGoogleMapsApi() {
     return loadGoogleMapsApi({
-      key: 'AIzaSyBhF6NIX0O2cmBMx3IpHtrvkyc09YfioMg',
+      key: 'AIzaSyDIfb1nXWDg2kwRNTseTnBuIcPrB-2DqMM',
       libraries: ['places'],
     });
   }
